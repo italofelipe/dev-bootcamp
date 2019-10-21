1904ded07f082bff05e9d14099f0b2f36e468f60
italofelipe <felipe.italo@hotmail.com>
2019-10-21 14:35:35 -0300

    Aula nº 14: Configuradas algumas rotas, passadas respostas que o servidor devolve ao cliente.

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
 const express = require("express");
 const dotenv = require("dotenv");
 // Load .ENV vars
+
+// String padrão de rotas
+const defaultRoute = "/api/v1";
 dotenv.config({ path: "./config/config.env" });
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.get("/", (req, res) => {
-	res.send("Hello Express!");
+
+// GET
+app.get(`${defaultRoute}/bootcamps`, (req, res) => {
+	res.status(200).json({ success: true, msg: "Showing a list of botocamps." });
 });
 
+// GET POR ID
+app.get(`${defaultRoute}/bootcamps/:id`, (req, res) => {
+	res
+		.status(200)
+		.json({ success: true, msg: `Showing boot camp ${req.params.id}` });
+});
+
+// POST
+app.post(`${defaultRoute}/bootcamps`, (req, res) => {
+	res.status(200).json({ success: true, msg: "Create a botocamps." });
+});
+
+// PUT
+app.put(`${defaultRoute}/bootcamps/:id`, (req, res) => {
+	res.status(200).json({ success: true, msg: "Updating a bootcamp." });
+});
+
+// DELETE
+app.delete(`${defaultRoute}/bootcamps/:id`, (req, res) => {
+	res.status(200).json({ success: true, msg: "Delete a bootcamp." });
+});
 app.listen(PORT, () => {
 	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
