a533641ac96c9c55bd74d68c7b0131009a3b3874
italofelipe <felipe.italo@hotmail.com>
2019-11-07 17:16:07 -0300

    Fim da aula 38 - Implementada a busca por cursos. A rota de cursos foi implementado tendo como âncora a rota de bootcamps.

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -12,6 +12,16 @@ const {
 	getBootcampsInRadius
 } = require("../controllers/bootcampsController");
 
+// Incluir rotas de outros Recursos
+/*
+	O que estou fazendo aqui: com a linha abaixo, estou basicamente falando que, quando a rota
+	começar com "bootcampId/courses", o express vai me redirecionar para a rota de cursos
+	Isso é necessário por causa do relacionamento entre bootcamps e cursos.
+
+*/
+const courseRouter = require("./courses");
+Router.use("/:bootcampId/courses", courseRouter);
+
 Router.route("/")
 	.get(getBootcamps)
 	.post(createBootcamp);
--- routes/courses.js
+++ routes/courses.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const { getCourses } = require("../controllers/coursesController");
 
-const router = express.Router();
+/*  Esse objeto com mergeParams é necessário por conta do relacionamento com bootcamps
+ *   A rota para obter os cursos provém dos Bootcamps (ver arquivo bootcamps, nessa mesma pasta)
+ */
+const router = express.Router({ mergeParams: true });
 router.route("/").get(getCourses);
 module.exports = router;
