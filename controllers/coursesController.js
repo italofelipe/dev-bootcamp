3060f3a47ff1d6f870f7f7946777b00c992c6fa8
ItaloFelipe <felipe.italo@hotmail.com>
2020-01-27 23:23:56 -0300

    Fim da aula 40 - Implementado metodo pra crar um curso dentro de um Bootcamp especifico.

--- controllers/coursesController.js
+++ controllers/coursesController.js
@@ -1,4 +1,5 @@
 const Course = require('../models/Courses');
+const Bootcamp = require('../models/Bootcamp');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middlewares/async');
 
@@ -27,7 +28,44 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
 	});
 });
 
-/* Desc: Obter todos os bootcamps
- *  ROTA: GET /api/v1/bootcamps
+/* Desc: Obter um unico Course
+ *  ROTA: GET /api/v1/courses/:id
  *  ACESSO: Public
  */
+
+exports.getCourse = asyncHandler(async (req, res, next) => {
+	const course = await Course.findById(req.params.id).populate({
+		path: 'bootcamp',
+		select: 'name description'
+	});
+
+	if (!course) {
+		return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404);
+	}
+	res.status(200).json({
+		success: true,
+		data: course
+	});
+});
+
+/* Desc: Criar um Course
+ *  ROTA: POST /api/v1/bootcamps/:bootCampId/courses
+ *  ACESSO: Private
+ */
+
+exports.addCourse = asyncHandler(async (req, res, next) => {
+	req.body.bootcamp = req.params.bootcampId;
+
+	const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+	if (!bootcamp) {
+		return next(new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`), 404);
+	}
+
+	const course = await Course.create(req.body);
+
+	res.status(200).json({
+		success: true,
+		data: course
+	});
+});
--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,10 @@
-const express = require("express");
-const { getCourses } = require("../controllers/coursesController");
+const express = require('express');
+const { getCourses, getCourse, addCourse } = require('../controllers/coursesController');
 
 /*  Esse objeto com mergeParams é necessário por conta do relacionamento com bootcamps
  *   A rota para obter os cursos provém dos Bootcamps (ver arquivo bootcamps, nessa mesma pasta)
  */
 const router = express.Router({ mergeParams: true });
-router.route("/").get(getCourses);
+router.route('/').get(getCourses).post(addCourse);
+router.route('/:id').get(getCourse);
 module.exports = router;
